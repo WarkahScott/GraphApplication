a996bd86ae38cc704d8fb57f6b9af7e569a11444
Warkah Scott <warkahscott@gmail.com>
2019-03-04 04:21:50 -0500

    Added mostly visual menu

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -2,12 +2,18 @@ import java.util.Collections;
 import java.util.LinkedList;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.*;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
@@ -21,6 +27,8 @@ public class MainGUI extends Application
 {
 	private final int SCREEN_WIDTH = 900;
 	private final int SCREEN_HEIGHT = 750;
+	private final int CIRCLE_RADIUS = 10;
+	private final VBox MENU = new VBox(15);
 	
 	private LinkedList<Vertex> nodes = new LinkedList<Vertex>();
 	private LinkedList<Line> edges = new LinkedList<Line>();
@@ -33,7 +41,7 @@ public class MainGUI extends Application
 	@Override
 	public void start(Stage mainStage) throws Exception
 	{
-		mainStage.setTitle("Graph Application v0.2");
+		mainStage.setTitle("Graph Application v0.3");
 		mainStage.getIcons().add(new Image(MainGUI.class.getResourceAsStream("graph.jpg")));
 		
 		//Centers the window
@@ -43,7 +51,39 @@ public class MainGUI extends Application
 		
 		mainStage.setResizable(true);
 		mainStage.setHeight(SCREEN_HEIGHT);
-		mainStage.setWidth(SCREEN_WIDTH);
+		mainStage.setWidth(SCREEN_WIDTH + 100);
+		
+		MENU.setPrefWidth(100);
+		
+		Button organize = new Button("Organize");
+		organize.setPrefWidth(80);
+		organize.setOnAction(e -> 	{
+										organize();
+										draw(mainStage);
+									});
+		
+		ToggleGroup profiles = new ToggleGroup();
+		RadioButton r1 = new RadioButton("Degree");
+		r1.setToggleGroup(profiles);
+		RadioButton r2 = new RadioButton("I. Max");
+		r2.setToggleGroup(profiles);
+		RadioButton r3 = new RadioButton("E. Max");
+		r3.setToggleGroup(profiles);
+		RadioButton r4 = new RadioButton("I. Min");
+		r4.setToggleGroup(profiles);
+		RadioButton r5 = new RadioButton("E. Min");
+		r5.setToggleGroup(profiles);
+		VBox profileMenu = new VBox(5, r1, r2, r3, r4, r5);
+		
+		Button clear = new Button("Clear");
+		clear.setPrefWidth(80);
+		clear.setOnAction(e -> 	{
+										clear();
+										draw(mainStage);
+									});
+		
+		MENU.getChildren().addAll(organize, profileMenu, clear);
+		MENU.setAlignment(Pos.CENTER);
 		
 		draw(mainStage);
 		mainStage.show();
@@ -54,7 +94,8 @@ public class MainGUI extends Application
 		String profileText = "";
 		Collections.sort(nodes, Collections.reverseOrder());
 		
-		Group scene = new Group();
+		Pane scene = new Pane();
+		scene.setPrefSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 		
 		for(Line e : edges)
 		{
@@ -65,7 +106,7 @@ public class MainGUI extends Application
 		
 		for(Vertex v : nodes)
 		{
-			Circle vertex = new Circle(v.getX(), v.getY(), 10);
+			Circle vertex = new Circle(v.getX(), v.getY(), CIRCLE_RADIUS);
 			scene.getChildren().add(vertex);
 			Text label = new Text(v.getX() - 5, v.getY() + 5, "" + v.getConnections().size());
 			label.setFill(Color.WHITE);
@@ -74,28 +115,30 @@ public class MainGUI extends Application
 			profileText = profileText + v.getConnections().size() + " ";
 		}
 		
-		Text profile = new Text(10, 700, "Profile: " + profileText);
+		Text profile = new Text(10, owner.getHeight()-50, "Profile: " + profileText);
 		profile.setFill(Color.BLACK);
 		profile.setFont(Font.font("arial", FontWeight.BOLD, null, 20));
 		scene.getChildren().add(profile);
 		
-		Scene canvas = new Scene(scene);
-		canvas.addEventFilter(MouseEvent.MOUSE_PRESSED, e -> 	{
-			startX = e.getSceneX(); 
-			startY = e.getSceneY();	
+		HBox gui = new HBox(5, MENU, scene);
+		Scene screen = new Scene(gui);
+		
+		scene.addEventFilter(MouseEvent.MOUSE_PRESSED, e -> 	{
+			startX = e.getX(); 
+			startY = e.getY();	
 		});
-		canvas.addEventFilter(MouseEvent.MOUSE_RELEASED, e -> 	{
-			endX = e.getSceneX(); 
-			endY = e.getSceneY();
+		scene.addEventFilter(MouseEvent.MOUSE_RELEASED, e -> 	{
+			endX = e.getX(); 
+			endY = e.getY();
 		});
 
-		canvas.setOnMouseClicked(e -> 	{
+		scene.setOnMouseClicked(e -> 	{
 											if(e.isStillSincePress())
 											{
 												if(e.getButton() == MouseButton.PRIMARY)
-													add(e.getSceneX(), e.getSceneY());
+													add(e.getX(), e.getY());
 												if(e.getButton() == MouseButton.SECONDARY)
-													remove(e.getSceneX(), e.getSceneY());
+													remove(e.getX(), e.getY());
 											}
 											else
 												if(e.getButton() == MouseButton.PRIMARY)
@@ -107,16 +150,16 @@ public class MainGUI extends Application
 		Vertex.setIndex(nodes.size());
 		for(Vertex v : nodes)
 			v.setDegree(nodes.size() - v.getConnections().size());
-		
-		owner.setScene(canvas);
+
+		owner.setScene(screen);
 	}
 	
 	private void add(double x, double y)
 	{
-		Circle newV = new Circle(x, y, 10);
+		Circle newV = new Circle(x, y, CIRCLE_RADIUS);
 		for(Vertex v : nodes)
 		{
-			Circle old = new Circle(v.getX(), v.getY(), 10);
+			Circle old = new Circle(v.getX(), v.getY(), CIRCLE_RADIUS);
 			if(old.intersects(newV.getBoundsInLocal()))
 				return;
 		}
@@ -125,14 +168,14 @@ public class MainGUI extends Application
 	}
 	
 	private void add(double x, double y, double x2, double y2)
-	{
+	{	
 		Vertex v1 = null;
 		Vertex v2 = null;
 		
 		//Checks if end is in a vertex
 		for(Vertex v : nodes)
 		{
-			Circle tmp = new Circle(v.getX(), v.getY(), 10);
+			Circle tmp = new Circle(v.getX(), v.getY(), CIRCLE_RADIUS);
 			if(tmp.contains(x2, y2))
 			{
 				v2 = v;
@@ -144,7 +187,7 @@ public class MainGUI extends Application
 		{
 			for(Vertex v : nodes)
 			{
-				Circle tmp = new Circle(v.getX(), v.getY(), 10);
+				Circle tmp = new Circle(v.getX(), v.getY(), CIRCLE_RADIUS);
 				if(!v.equals(v2) && tmp.contains(x, y))
 				{
 					v1 = v;
@@ -167,10 +210,10 @@ public class MainGUI extends Application
 		//If not attempt a move
 		else
 		{
-			Circle validator = new Circle(x2, y2, 10);
+			Circle validator = new Circle(x2, y2, CIRCLE_RADIUS);
 			for(Vertex v : nodes)
 			{
-				Circle tmp = new Circle(v.getX(), v.getY(), 10);
+				Circle tmp = new Circle(v.getX(), v.getY(), CIRCLE_RADIUS);
 				
 				if(validator.intersects(tmp.getBoundsInLocal()))
 					return;
@@ -178,8 +221,10 @@ public class MainGUI extends Application
 					v1 = v;
 			}
 			
+			if(v1 == null)
+				return;
 			for(Line e : edges)
-				if(e.contains(x, y) || e.contains(x2, y2))
+				if(e.contains(x, y) && !new Circle(v1.getX(), v1.getY(), CIRCLE_RADIUS).contains(x, y) || e.contains(x2, y2))
 					return;
 			
 			for(Line e : edges)
@@ -206,7 +251,7 @@ public class MainGUI extends Application
 	{
 		for(Vertex v : nodes)
 		{
-			Circle check = new Circle(v.getX(), v.getY(), 10);
+			Circle check = new Circle(v.getX(), v.getY(), CIRCLE_RADIUS);
 			if(check.contains(x, y))
 			{
 				LinkedList<Vertex> rmv = new LinkedList<Vertex>();
@@ -248,6 +293,45 @@ public class MainGUI extends Application
 				return;
 			}
 		}
+	}
+	
+	private void organize()
+	{
+		Collections.sort(nodes, Collections.reverseOrder());
+		final int DISTANCE = 300;
+		double circleHelper = (Math.PI * 2) / nodes.size();
+		int count = 0;
+		for(Vertex v : nodes)
+		{
+			int tmpX = (int) (SCREEN_WIDTH/2 + DISTANCE * Math.cos(circleHelper * count));
+			int tmpY = (int) (SCREEN_HEIGHT/2 + DISTANCE * Math.sin(circleHelper * count));
+			
+			for(Line e : edges)
+			{
+				if(e.getStartX() == v.getX() && e.getStartY() == v.getY())
+				{
+					e.setStartX(tmpX);
+					e.setStartY(tmpY);
+				}
+				
+				if(e.getEndX() == v.getX() && e.getEndY() == v.getY())
+				{
+					e.setEndX(tmpX);
+					e.setEndY(tmpY);
+				}
+			}
+			
+			v.setX(tmpX);
+			v.setY(tmpY);
+			
+			count++;
+		}
+	}
+	
+	private void clear()
+	{
+		nodes = new LinkedList<Vertex>();
+		edges = new LinkedList<Line>();
 	}
 	
 	public static void main(String[] args)
